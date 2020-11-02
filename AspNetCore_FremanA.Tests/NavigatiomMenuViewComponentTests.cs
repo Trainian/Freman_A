ea5b25473b9c93652ba3c05f8e42afb84522a795
Дмитрий Горбовский <dimon007@inbox.ru>
2020-11-02 19:47:41 +0300

    Init. Site with Tests

---
+++ AspNetCore_FremanA.Tests/NavigatiomMenuViewComponentTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AspNetCore_FremanA.Components;
+using AspNetCore_FremanA.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using Xunit;
+
+namespace AspNetCore_FremanA.Tests
+{
+    public class NavigatiomMenuViewComponentTests
+    {
+
+        [Fact]
+        public void Can_Select_Categories()
+        {
+            // Организация
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product() {ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product() {ProductID = 2, Name = "P2", Category = "Apples"},
+                new Product() {ProductID = 3, Name = "P3", Category = "Plums"},
+                new Product() {ProductID = 4, Name = "P4", Category = "Oranges"},
+            }).AsQueryable<Product>());
+
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+
+            // Действие- получение набора категорий
+            string[] results = ((IEnumerable<string>) (target.Invoke() as ViewViewComponentResult)?.ViewData.Model)?.ToArray();
+
+            // Утверждение
+            Assert.True(Enumerable.SequenceEqual(new string[] {"Apples", "Oranges", "Plums"}, results));
+        }
+
+        [Fact]
+        public void Indicates_Selected_Category()
+        {
+            // Организация
+            string categoryToSelect = "Apples";
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product() {ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product() {ProductID = 4, Name = "P2", Category = "Oranges"}
+            }).AsQueryable<Product>());
+
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+            target.ViewComponentContext = new ViewComponentContext()
+            {
+                ViewContext = new ViewContext()
+                {
+                    RouteData = new RouteData()
+                }
+            };
+            target.RouteData.Values["category"] = categoryToSelect;
+
+            // Действие
+            string result = (string) (target.Invoke() as ViewViewComponentResult)?.ViewData["SelectedCategory"];
+
+            // Утверждение
+            Assert.Equal(categoryToSelect, result);
+        }
+    }
+}
---
+++ AspNetCore_FremanA.Tests/OrderControllerTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AspNetCore_FremanA.Controllers;
+using AspNetCore_FremanA.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace AspNetCore_FremanA.Tests
+{
+    public class OrderControllerTests
+    {
+        [Fact]
+        public void Cannot_Checkout_Empty_Cart()
+        {
+            // Организация -создание имитированного хранилища заказов
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+
+            // Органзация - соаздение пустой корзины
+            Cart cart = new Cart();
+
+            // Организация - создание заказа
+            Order order = new Order();
+
+            // Организация - создание экземпляра контроллера
+            OrderController target = new OrderController(mock.Object, cart);
+
+            // Действие
+            ViewResult result = target.Checkout(order) as ViewResult;
+
+            // Утверждение - проверка, что заказ был сохранен
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+
+            // Утверждение - проверка, что метод возвращает стандартное предстваление
+            Assert.True(string.IsNullOrEmpty(result.ViewName));
+
+            // Утверждение - проврка, что представлению передана недопустимая модель
+            Assert.False(result.ViewData.ModelState.IsValid);
+        }
+
+        [Fact]
+        public void Cannot_Checkout_Invalid_ShippingDetails()
+        {
+            // Организация - создание имитированного хранилища заказов
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+
+            // Организация - создание корзины с одним элементом
+            Cart cart = new Cart();
+            cart.AddItem(new Product(), 1);
+
+            // Организация - создание жкземпляра контроллера
+            OrderController target = new OrderController(mock.Object, cart);
+
+            // Организация - добавление ошибки в модель
+            target.ModelState.AddModelError("error", "error");
+
+            // Действие - попытка перехода к оплате
+            ViewResult result = target.Checkout(new Order()) as ViewResult;
+
+            // Утверждение - проверка, что заказ не был сохранен
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+
+            // Утверждение - проверка, что метод возвращает стандартное представление
+            Assert.True(string.IsNullOrEmpty(result.ViewName));
+
+            // Утверждение - проверка, что представлению передаётся недопустимая модель
+            Assert.False(result.ViewData.ModelState.IsValid);
+        }
+
+        [Fact]
+        public void Can_Checkout_And_Submit_Order()
+        {
+            // Организация - создание имитированного хранилища заказов
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+
+            // Организация - создание корзины с одним элементом
+            Cart cart = new Cart();
+            cart.AddItem(new Product(), 1);
+
+            // Организация - создание экземпляра контроллера
+            OrderController target = new OrderController(mock.Object, cart);
+
+            // Действие - попытка перехода к оплате
+            RedirectToActionResult result = target.Checkout(new Order()) as RedirectToActionResult;
+
+            // Утверждение - проверка, что заказ был сохранен
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Once);
+
+            // Утверждение - проверка, что метод перенаправляется на действие Completed
+            Assert.Equal("Completed", result.ActionName);
+        }
+    }
+}
